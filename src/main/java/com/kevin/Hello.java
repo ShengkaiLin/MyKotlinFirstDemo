590ab2b9321d6f1f0d36a55f4ccb64464131aa10
ShengKaiLin <yangmin31109@gmail.com>
2023-01-31 17:26:16 +0800

    Initial commit

---
+++ src/main/java/com/kevin/Hello.java
@@ -0,0 +1,12 @@
+package com.kevin;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        //System.out.println("Hello Java!");
+
+        Person person = new Person();
+        person.hello();
+
+    }
+}
